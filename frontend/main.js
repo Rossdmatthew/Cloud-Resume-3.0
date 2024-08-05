97c055219028b0ab4ac831ad5632da73d7c7975a
Rossdmatthew <146154563+Rossdmatthew@users.noreply.github.com>
2024-08-05 01:07:41 -0500

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApiUrl = 'https://getmingcounter.azurewebsites.net/api/GetResumeCounter?code=wHlWN0FpKRt_CfcR5I3UA4352es5cax1gIOLPOiFQycfAzFuZ4z6Sg%3D%3D'
+const functionApiUrl = 'https://rdmgetresumecounter.azurewebsites.net/api/GetResumeCounter?code=ydoX7QuyG7FppfQOtuPBUfFmO_nBViu-T0PIEKOvFTp_AzFu1B7EjQ%3D%3D'
 const localFunctionApi ='http://localhost:7071/api/GetResumeCounter'
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(localFunctionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
